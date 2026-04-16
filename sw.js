369964d8a35f368d1116e9e7681b1df1b6e8a1ae
ydgin <ydginnuch@gmail.com>
2026-04-16 16:15:38 +0300

    change

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pls-invest-v1';
+const urlsToCache = [
+    '/meter-counter/',
+    '/meter-counter/index.html',
+    '/meter-counter/style.css',
+    '/meter-counter/script.js',
+    '/meter-counter/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
